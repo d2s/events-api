35a8b7e9927e95b7fbd4f72e37a53ef4e7536034
Daniel Schildt <daniel@autiomaa.org>
2020-04-15 05:10:54 +0300

    style: improve code readability with Prettier
    Improve code readability with Prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 module.exports = async () => {
-  const request = await fetch('https://api.github.com/users/d2s')
-  const data = await request.json()
+  const request = await fetch("https://api.github.com/users/d2s");
+  const data = await request.json();
 
-  return data
-}
+  return data;
+};
